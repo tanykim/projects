1ae1fc2abca200e318cb1f75f1ecead3cbcd74db
Tanyoung Kim <tanyoung.kim@airbnb.com>
2023-10-14 12:04:08 -0700

    Add best bookshelf and overlap

---
+++ overlaps-of-the-three/precache-manifest.15590981dceda52c3768aa68604b7fc2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "34021b1d07b9e67f7867",
+    "url": "/static/css/main.939e06e7.chunk.css"
+  },
+  {
+    "revision": "34021b1d07b9e67f7867",
+    "url": "/static/js/main.34021b1d.chunk.js"
+  },
+  {
+    "revision": "807368dc94bef7c57fe0",
+    "url": "/static/css/1.b18c2344.chunk.css"
+  },
+  {
+    "revision": "807368dc94bef7c57fe0",
+    "url": "/static/js/1.807368dc.chunk.js"
+  },
+  {
+    "revision": "01c4f2f84e7372d099a5",
+    "url": "/static/js/runtime~main.01c4f2f8.js"
+  },
+  {
+    "revision": "2c58b2a374c30af73b4e725cf8a4d3a8",
+    "url": "/static/media/warning.2c58b2a3.svg"
+  },
+  {
+    "revision": "c92594a601458bdba9b7193bcb7fd927",
+    "url": "/static/media/warning-disabled.c92594a6.svg"
+  },
+  {
+    "revision": "06ce64396c471bb4ac02e27f59012c5a",
+    "url": "/static/media/arrow-up-fb.06ce6439.svg"
+  },
+  {
+    "revision": "84152226a68cfc727f9fbdd64681605c",
+    "url": "/static/media/arrow-up.84152226.svg"
+  },
+  {
+    "revision": "eb680b8dec0ec66210db9217c1397c6b",
+    "url": "/static/media/arrow-up-ig.eb680b8d.svg"
+  },
+  {
+    "revision": "7ef7bc1687af7316b25bbb370284f190",
+    "url": "/static/media/arrow-up-wa.7ef7bc16.svg"
+  },
+  {
+    "revision": "c5ceed16390d0c9bf37e82fd7e289050",
+    "url": "/static/media/arrow-down.c5ceed16.svg"
+  },
+  {
+    "revision": "b0d26299d04daadecca00565f0115836",
+    "url": "/static/media/close.b0d26299.svg"
+  },
+  {
+    "revision": "6ecf786fee74365390779474b57321c4",
+    "url": "/static/media/play.6ecf786f.svg"
+  },
+  {
+    "revision": "2656640e0b5c4b14e4653e255c8f3dee",
+    "url": "/static/media/play-hover.2656640e.svg"
+  },
+  {
+    "revision": "4e55f45161e000a3ccc6b65fe992ee62",
+    "url": "/static/media/pause.4e55f451.svg"
+  },
+  {
+    "revision": "9dc3523f40d3e084f6b9569d2b097160",
+    "url": "/static/media/pause-hover.9dc3523f.svg"
+  },
+  {
+    "revision": "e7c88ce245be5f9d5b3e495c1f4d4c90",
+    "url": "/static/media/FreigSanLFProBoo.e7c88ce2.otf"
+  },
+  {
+    "revision": "b80c26a4339ac6add1107d2d3eab5f8a",
+    "url": "/index.html"
+  }
+];
