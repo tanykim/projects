899966cf4857576973f7ecd52fbc643c45ea1728
Tanyoung Kim <tanyoung.kim@airbnb.com>
2024-02-06 22:08:20 -0800

    Add quantify-your-year and shades-of-exercise

---
+++ quantify-your-year/precache-manifest.12660abc2ee7862feb4a1112b0c72dc9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f90410aeec9b94bca0b9362a04af01",
+    "url": "/quantify-your-year/index.html"
+  },
+  {
+    "revision": "2c0ecf63c77f114233b0",
+    "url": "/quantify-your-year/static/css/2.39e6be60.chunk.css"
+  },
+  {
+    "revision": "db93f6bdec85863b97ab",
+    "url": "/quantify-your-year/static/css/main.4f2fdfcc.chunk.css"
+  },
+  {
+    "revision": "2c0ecf63c77f114233b0",
+    "url": "/quantify-your-year/static/js/2.203b1578.chunk.js"
+  },
+  {
+    "revision": "db93f6bdec85863b97ab",
+    "url": "/quantify-your-year/static/js/main.7a9ddab8.chunk.js"
+  },
+  {
+    "revision": "60d7c2cb565309700791",
+    "url": "/quantify-your-year/static/js/runtime~main.9307e79e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/quantify-your-year/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/quantify-your-year/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/quantify-your-year/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/quantify-your-year/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/quantify-your-year/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/quantify-your-year/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/quantify-your-year/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/quantify-your-year/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/quantify-your-year/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/quantify-your-year/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
---
+++ shades-of-exercise/precache-manifest.48888d78cfeab2c6809a6d9e7beb0505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f2521137867011f8e9b3faf33b1ba9",
+    "url": "/exercise/index.html"
+  },
+  {
+    "revision": "603ae41420faee2f9f7b",
+    "url": "/exercise/static/css/main.ad430f13.chunk.css"
+  },
+  {
+    "revision": "0125dc2a3edc8e8deb92",
+    "url": "/exercise/static/js/2.0a60808c.chunk.js"
+  },
+  {
+    "revision": "603ae41420faee2f9f7b",
+    "url": "/exercise/static/js/main.a77d57d2.chunk.js"
+  },
+  {
+    "revision": "8470adb75994f5f8af91",
+    "url": "/exercise/static/js/runtime~main.ba9c4bf5.js"
+  }
+]);
